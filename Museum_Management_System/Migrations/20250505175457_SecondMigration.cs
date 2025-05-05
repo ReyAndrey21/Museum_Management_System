6307acc07eeb916d68e4487f52441e2e2b59b154
ReyAndrey21 <bitoleanu.eugen.f5a@student.ucv.ro>
2025-05-05 21:13:32 +0300

    Add second migration

---
+++ Museum_Management_System/Migrations/20250505175457_SecondMigration.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Museum_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "hour_tour",
+                table: "Tours",
+                type: "time without time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "date_tour",
+                table: "Tours",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "start_hour",
+                table: "TourGuideSchedules",
+                type: "time without time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "end_hour",
+                table: "TourGuideSchedules",
+                type: "time without time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "purchase_date",
+                table: "Tickets",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "date_review",
+                table: "Reviews",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "opening_hour",
+                table: "MuseumSchedules",
+                type: "time without time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "closing_hour",
+                table: "MuseumSchedules",
+                type: "time without time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "hire_date",
+                table: "Employees",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "hour_tour",
+                table: "Tours",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "date_tour",
+                table: "Tours",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "start_hour",
+                table: "TourGuideSchedules",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time without time zone");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "end_hour",
+                table: "TourGuideSchedules",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "purchase_date",
+                table: "Tickets",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "date_review",
+                table: "Reviews",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "opening_hour",
+                table: "MuseumSchedules",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time without time zone");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "closing_hour",
+                table: "MuseumSchedules",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "hire_date",
+                table: "Employees",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
