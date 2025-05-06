0e2b7f570d9066afcc57bb03e86d8fd2c39ae704
ReyAndrey21 <bitoleanu.eugen.f5a@student.ucv.ro>
2025-05-06 22:23:01 +0300

    Create layout for login-register system

---
+++ Museum_Management_System/Views/Shared/UsersAuthLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Museum_Management_System.Views.Shared
+{
+    public class UserAuthLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
