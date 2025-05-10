9e1db9eecd7b14b3b128fe9637bf8c0305589bfd
ReyAndrey21 <bitoleanu.eugen.f5a@student.ucv.ro>
2025-05-10 19:04:53 +0300

    Modify Ticket model

--- Museum_Management_System/Models/Ticket.cs
+++ Museum_Management_System/Models/Ticket.cs
@@ -9,16 +9,17 @@ public class Ticket
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Column("id_ticket")]
     public int IdTicket { get; set; }
-    [Column("price")]
-    public double Price { get; set; }
     [Column("purchase_date")]
     [DataType(DataType.Date)]
     public DateOnly PurchaseDate { get; set; }
-    [Column("ticket_type")]
-    public string? TicketType { get; set; }
+    [Column("final_price")]
+    public double FinalPrice { get; set; }
+    [ForeignKey("TicketType")]
+    [Column("id_ticket_type")]
+    public int IdTicketType { get; set; }
+    public TicketType? TicketType { get; set; }
     [ForeignKey("Users")]
     [Column("id_users")]
-
     public int IdUsers { get; set; }
     public Users? User { get; set; }
 
