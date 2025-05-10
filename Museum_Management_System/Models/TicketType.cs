6e30fc86637b251861b98a672ad4d0ed3124ac64
ReyAndrey21 <bitoleanu.eugen.f5a@student.ucv.ro>
2025-05-10 19:04:00 +0300

    Modify Ticket model

---
+++ Museum_Management_System/Models/TicketType.cs
@@ -0,0 +1,19 @@
+﻿namespace Museum_Management_System.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+public class TicketType
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id_ticket_type")]
+    public int IdTicketType { get; set; }
+
+    [Column("type_name")]
+    public string? TypeName { get; set; }
+
+    [Column("base_price")]
+    public double BasePrice { get; set; }
+
+    public ICollection<Ticket>? Tickets { get; set; }
+}
